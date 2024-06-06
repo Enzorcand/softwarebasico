5717e0d649782425ecbe230ce0f7148feaafe497
Enzorcand <83414193+Enzorcand@users.noreply.github.com>
2024-06-06 22:30:01 +0000

    código da zoom porem possui linhas falhas

--- zoom.c
+++ zoom.c
@@ -44,10 +44,9 @@ int main() {
             int blue = image_in->pix[k*image_in->width + m].b;
             for (int l = 0; l < 3; l++){
                 for(int n = 0; n < 3; n++){
-                    image_out->pix[(j + l)*image_in->width + i + n].r = red;
-                    image_out->pix[(j + l)*image_in->width + i + n].g = green;
-                    image_out->pix[(j + l)*image_in->width + i + n].b = blue;
-                    
+                    image_out->pix[(j)*image_in->width + i + n].r = red;
+                    image_out->pix[(j)*image_in->width + i + n].g = green;
+                    image_out->pix[(j)*image_in->width + i + n].b = blue;
                 }
             }
             i += 3;
@@ -68,102 +67,6 @@ int main() {
     }
 }
 
-int read_ppm(char *file, struct image_s *image)
-{
-	FILE *fptr;
-	char *t;
-	char buf[128];
-	int r, d, i, j;
-	struct pixel_s *img_data;
-	
-	fptr = fopen(file, "rb");
-	if (!fptr)
-		return -1;
-		
-	t = fgets(buf, sizeof(buf), fptr);
-	if (!t)
-		goto failread;
-	
-	if (strncmp(buf, "P6\n", 3) != 0)
-		goto failread;
-
-        do {
-		t = fgets(buf, sizeof(buf), fptr);
-		if (!t)
-			goto failread;
-				
-	} while (strncmp(buf, "#", 1) == 0);
-	
-        r = sscanf(buf, "%u %u", &image->width, &image->height);
-        if (r < 2)
-		goto failread;
-
-        r = fscanf(fptr, "%u", &d);
-        if ((r < 1) || (d != 255))
-		goto failread;
-		
-        fseek(fptr, 1, SEEK_CUR);
-        
-        img_data = malloc(image->width * image->height * sizeof(struct pixel_s));
-        if (!img_data)
-		goto failread;
-        
-        image->pix = img_data;
-        
-        for (j = 0; j < image->height; j++)
-		for (i = 0; i < image->width; i++)
-			fread(&img_data[j * image->width + i], 3, 1, fptr);
-	
-	fclose(fptr);
-
-	return 0;
-	
-failread:
-	fclose(fptr);
-	
-	return -1;
-}
-
-
-int write_ppm(char *file, struct image_s *image)
-{
-	FILE *fptr;
-	int i, j;
-	
-	fptr = fopen(file, "wb");
-	if (!fptr)
-		return -1;
-		
-	fprintf(fptr, "P6\n%d %d\n255\n", image->width, image->height);
-        for (j = 0; j < image->height; j++)
-		for (i = 0; i < image->width; i++)
-			fwrite(&image->pix[j * image->width + i], 3, 1, fptr);
-
-	fclose(fptr);
-	
-	return 0;
-}
-
-
-int new_ppm(struct image_s *image, int width, int height)
-{
-	struct pixel_s *img_data;
-
-	image->width = width;
-	image->height = height;
-	
-        img_data = malloc(image->width * image->height * sizeof(struct pixel_s));
-        if (!img_data)
-		return -1;
-        
-        image->pix = img_data;
-        memset(image->pix, 0, image->width * image->height * sizeof(struct pixel_s));
-	
-	return 0;
-}
-
-
-int free_ppm(struct image_s *image)
 {
 	if (image->pix) {
 		free(image->pix);
